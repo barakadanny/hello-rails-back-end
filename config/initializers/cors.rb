2719fa26510992e632d3a2837c45c1c680423364
Danny <barakadan421@gmail.com>
2023-02-04 14:32:12 +0300

    Solve Linters 1

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
     resource '*',
-    headers: :any,
-    methods: [:get, :post, :put, :patch, :delete, :options, :head],
-    credentials: false
+             headers: :any,
+             methods: [:get, :post, :put, :patch, :delete, :options, :head],
+             credentials: false
   end
 end
